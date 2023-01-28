14941b38ef7f1d315127272c0de53b0847f7d22b
Kimberly Dijkmans <kim.14420@outlook.com>
2023-01-28 23:08:10 +0000

    implemented application and context menus

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { app, BrowserWindow, ipcMain, Notification } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, MenuItem  } = require('electron');
 
 const isMac = process.platform === 'darwin'; // Check if platform is a Mac (darwin: mac, win32: windows, linux: linux)
 
@@ -180,7 +180,7 @@ ipcMain.on('copy:result', (e, result) => {
 let mainWindow;
 function createMainWindow() {
     mainWindow = new BrowserWindow({
-        title: "Electron app with ReactJS",
+        title: "Tekst opmaken",
         width: isDev ? 1300 : 1000,
         height: 800,
         backgroundColor: "white",
@@ -199,12 +199,67 @@ function createMainWindow() {
 }
 
 /*
+---- CREATE MENUS ----
+*/
+
+// Application menu (bar at top)
+const applicationMenuTemplate = [{
+    label: app.name,
+    submenu: [
+        // { label: 'About', click: createAboutWindow }, 
+        // { type: "separator" }, 
+        { label: 'Quit', accelerator: "Command+Q", click: () => app.quit() }
+    ]},{
+        label: "Edit",
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'delete' },
+            { type: 'separator' },
+            { role: 'selectAll' }
+    ]},{
+        label: "View",
+        submenu: [
+            { role: "reload" },
+            { role: "toggleDevTools" },
+            { type: "separator" },
+            { role: "resetzoom" },
+            { role: "zoomIn" },
+            { role: "zoomOut" },
+            { type: "separator" },
+            { role: "togglefullscreen" }
+        ]} 
+];
+
+// Context menu (right click)
+const contextMenuTemplate = [
+    { role: 'cut' },
+    { role: 'copy' },
+    { role: 'paste' },
+    { type: 'separator' },
+    { role: 'undo' },
+    { role: 'redo' },
+];
+
+/*
 ---- START AND STOP RUNNING APP ----
 */
 
 // App is ready
 app.whenReady().then(() => {
     createMainWindow();
+
+    // Implement menus
+    const applicationMenu = Menu.buildFromTemplate(applicationMenuTemplate);
+    Menu.setApplicationMenu(applicationMenu);
+    const contextMenu = Menu.buildFromTemplate(contextMenuTemplate);
+    mainWindow.webContents.on('context-menu', (e, params) => {
+        contextMenu.popup(mainWindow, params.x, params.y);
+    })
 
     // Remove mainWindow from memory on close
     mainWindow.on('closed', () => (mainWindow = null));
