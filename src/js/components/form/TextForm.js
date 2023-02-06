eb0893b762cced82706e3fcf7079db0c1f5d3f95
Kimberly Dijkmans <kim.14420@outlook.com>
2023-02-06 10:55:29 +0000

    added container for source field

--- src/js/components/form/TextForm.js
+++ src/js/components/form/TextForm.js
@@ -11,16 +11,18 @@ export default function TextForm({ unformattedText, source, updateFields }) {
                 required 
             />
 
-            <label>Bron van uitslag:</label>
-            <select
-                value ={source}
-                onChange={(e) => updateFields({source: e.target.value})}
-                required
-            >
-                <option disabled value={""}> -- selecteer -- </option>
-                <option value={"kbdb"}>KBDB</option>
-                <option value={"compuclub"}>Compuclub</option>
-            </select>
+            <div className="select-field-container">
+                <label>Bron van uitslag:</label>
+                <select
+                    value ={source}
+                    onChange={(e) => updateFields({source: e.target.value})}
+                    required
+                >
+                    <option disabled value={""}> -- selecteer -- </option>
+                    <option value={"kbdb"}>KBDB</option>
+                    <option value={"compuclub"}>Compuclub</option>
+                </select>
+            </div>
         </>
     )
 }
